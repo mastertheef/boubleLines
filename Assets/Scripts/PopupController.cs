06c0b0c4d3c58b7397800cb5eac4bf0f4c7cc4ed
Mykola <m.shvydkyy@abtollc.com>
2018-12-12 17:21:17 +0200

    settings popup in progress

---
+++ Assets/Scripts/PopupController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupController : MonoBehaviour {
+
+    [SerializeField] private Image popupDarken;
+    private Stack<Popup> popups;
+
+    private void Awake()
+    {
+        popups = new Stack<Popup>();
+    }
+
+    public void Show(Popup popup)
+    {
+        if (popups.Any())
+        {
+            var current = popups.Peek();
+            current.Hide();
+        }
+        popup.Show();
+        popups.Push(popup);
+        popupDarken.enabled = true;
+    }
+
+    public void Close()
+    {
+        if (popups.Any())
+        {
+            popups.Pop().Hide();
+            var prev = popups.Peek();
+            if (prev != null)
+            {
+                prev.Show();
+            }
+            else
+            {
+                popupDarken.enabled = false;
+            }
+        }
+        else
+        {
+            popupDarken.enabled = false;
+        }
+    }
+}
